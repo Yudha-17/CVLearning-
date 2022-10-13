4d6bb1dfaf4ff2ce2ee987dc3f03ba9e21fed40c
Yudha-17 <anakhilang22895@gmail.com>
2022-10-13 10:03:52 +0700

    Finally, I can access the IP Camera with this code. Make sure that your IP Camera and your computer are in the same connection

--- ipCamera.py
+++ ipCamera.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('http://192.168.43.216:8080/video.mjpg')
+cap = cv2.VideoCapture('http://192.168.75.157:8080/video')
 
 while True:
     ret, frame = cap.read()
     resized = cv2.resize(frame, (600, 400))
-    cv2.imshow("Android_cam", ret)
+    cv2.imshow("Android_cam", resized)
 
     # Press Esc key to exit
     if cv2.waitKey(1) == 27:
